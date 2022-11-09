0bcf115b22070f5a80d781407df29965ca5cf39f
Anbumani T <tanbumani1402@gmail.com>
2022-11-09 09:59:18 +0530

    first commit

---
+++ MongoRepository/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoRepository.Models;
+using UserApi.Services;
+
+namespace MongoRepository.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController:ControllerBase
+    {
+        private UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<List<User>> Get() =>
+            await _userService.GetAsync();
+
+        [HttpPost]
+        public async Task<ActionResult> Post(User newUser)
+        {
+            await _userService.CreateAsync(newUser);
+
+            return CreatedAtAction(nameof(Get), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(string id)
+        {
+            var user=await _userService.GetAsync(id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+
+    
+    }
+}
---
+++ MongoRepository/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoRepository.Models
+{
+    public class User
+    {
+        [BsonId]
+        
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
